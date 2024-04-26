fed8f6241bc080c1d3d29b9ec0cf556cc3fb6a86
Jack Chatelain <jackchatelain@gmail.com>
2024-04-26 17:19:12 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+i = input("Enter the number or range to check.\n")
+if "-" in i:
+    for i2 in range(int(i.split("-")[0]), int(i.split("-")[1]) + 1):
+        if int(i2) % 2 == 0: print(f"{i2} is even")
+        else: print(f"{i2} is odd")
+else:
+    if int(i) % 2 == 0: print(f"{i} is even")
+    else: print(f"{i} is odd")
